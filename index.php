3dab60b88204afa1811997c8df444ca337dcf183
lz-junior <correajr.dev@gmail.com>
2024-10-09 00:26:38 -0300

    var-dump

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-
   echo 'Hello World!';
+
 
   function foo() {
     echo 'foo';
   }
+  foo();
+  echo '</br>';
+
 
   class Teste {
     public function bar() {
       echo __METHOD__;
     }
   }
-
   echo '</br>';
 
-  foo();
-
-  echo '</br>';
 
   (new Teste)->bar(); 
+  $teste = new Teste;
+  var_dump($teste);
+  $teste->bar();
 
 ?>
